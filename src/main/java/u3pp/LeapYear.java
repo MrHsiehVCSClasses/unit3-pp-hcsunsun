5a50c711833ff214102a5e879d0a073aea6c5ff2
Sun <sun@MacBook-Pro-Sun.local>
2021-09-30 13:58:50 -0700

    submitting this

--- src/main/java/u3pp/LeapYear.java
+++ src/main/java/u3pp/LeapYear.java
@@ -2,7 +2,7 @@ package u3pp;
 
 /**
  * YOUR COMMENTs HERE
- * 
+ * checks if it is a leap year 
  * @author //YOUR NAME HERE
  *
  */
@@ -12,6 +12,30 @@ public class LeapYear {
 	 * YOUR COMMENTS HERE
 	 */
 	public static boolean isLeapYear(int year) {
-		/* YOUR CODE HERE */
+	
+		/* YOUR CODE HERE */   
+		if (year > 1582)
+		{
+			if (year % 100 != 0)
+			{
+				if (year % 4 == 0)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else if(year % 400 == 0)
+				{
+					return true;
+				}
+			else return false;
+				
+			
+		}else
+		return false;
+
 	}
 }
--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -1,10 +1,11 @@
 package u3pp;
 
+
 import java.util.Scanner;
 
 /**
  * YOUR COMMENTS HERE
- * 
+ * takes a string that user imput, check of it's (Stop), if not, check if it is a leap year using isLeapYear(), print out the outcome and ask agian untol the user say "stop"
  * @author YOUR NAME HERE
  */
 
@@ -13,15 +14,29 @@ class Main {
 	public static void main(String[] args) {
 
 		//YOUR CODE HERE
+		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Enter a year (or enter STOP to end): ");
 		String input = sc.nextLine();
 
+		
 		while (!input.toUpperCase().equals("STOP")) {
-			/* YOUR CODE HERE */
+
+			
+			int year = Integer.parseInt(input);
+
+			if (LeapYear.isLeapYear(year))
+			{
+				System.out.println(year + " is a leap year");
+			}
+			else{
+				System.out.println(year + " is not a leap year");
+			}
 			System.out.print("Enter a year (or enter STOP to end): ");
-			//YOUR CODE HERE
+			input = sc.nextLine();
+			
 		}
 		//YOUR CODE HERE
+		sc.close();
 	}
 }
